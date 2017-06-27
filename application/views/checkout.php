c8839785e7fc34511ab428c0977c8af67753b247
amin01kz <aminm96ir@gmail.com>
2017-06-27 22:39:35 +0430

    BS added in assets and fix small issue

--- application/views/checkout.php
+++ application/views/checkout.php
@@ -6,8 +6,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
 		<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>public/assets/css/style.css">
+	<link rel="stylesheet" href="<?=base_url()?>public/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?=base_url()?>public/assets/css/style.css">
 
 </head>
 <body>
@@ -73,6 +73,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script src="<?=base_url()?>public/assets/js/boostrap.min.js"></script>
 </body>
 </html>
