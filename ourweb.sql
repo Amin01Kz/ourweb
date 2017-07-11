ce782ad947bf6e25b9699bd9175f7fda89078688
amin01kz <aminm96ir@gmail.com>
2017-07-11 15:24:17 +0430

    SQL file updated

--- ourweb.sql
+++ ourweb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 27, 2017 at 10:00 PM
+-- Generation Time: Jul 11, 2017 at 03:23 PM
 -- Server version: 5.7.18-0ubuntu0.16.04.1
 -- PHP Version: 7.0.18-0ubuntu0.16.04.1
 
@@ -19,6 +19,20 @@ SET time_zone = "+00:00";
 --
 -- Database: `ourweb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE `contactus` (
+  `id` int(10) NOT NULL,
+  `subject` varchar(100) COLLATE utf8_bin NOT NULL,
+  `name` varchar(100) COLLATE utf8_bin NOT NULL,
+  `email` varchar(50) COLLATE utf8_bin NOT NULL,
+  `text` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -42,7 +56,8 @@ CREATE TABLE `factors` (
 --
 
 INSERT INTO `factors` (`id`, `code`, `receipt_code`, `name`, `description`, `phone`, `paid`, `date`) VALUES
-(2, 'asas', '123', 'asasas', 'dafsadfas', '09335657757', 0, '2017-06-27 17:08:49');
+(2, 'asas', '123', 'asasas', 'dafsadfas', '09335657757', 0, '2017-06-14 17:08:49'),
+(3, 'amin', '5664653', 'محمد امین', '', '+989335657757', 0, '2017-06-28 12:53:41');
 
 -- --------------------------------------------------------
 
@@ -62,11 +77,19 @@ CREATE TABLE `factor_items` (
 --
 
 INSERT INTO `factor_items` (`id`, `factor_id`, `title`, `price`) VALUES
-(1, 2, 'sdfsdfsdfsdf', 1000);
+(1, 2, 'sdfsdfsdfsdf', 1000),
+(2, 3, 'سیو فلان', 50000),
+(3, 3, 'قالب اماده', 10000);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `contactus`
+--
+ALTER TABLE `contactus`
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- Indexes for table `factors`
@@ -85,15 +108,20 @@ ALTER TABLE `factor_items`
 --
 
 --
+-- AUTO_INCREMENT for table `contactus`
+--
+ALTER TABLE `contactus`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `factors`
 --
 ALTER TABLE `factors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `factor_items`
 --
 ALTER TABLE `factor_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
